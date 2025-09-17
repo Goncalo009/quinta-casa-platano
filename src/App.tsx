b61242dfa044c42fb4223347bf8ec582fc219a4c
Goncalo009 <49418746+Goncalo009@users.noreply.github.com>
2025-09-17 20:08:26 +0100

    🎨 Add main React components - main.tsx, App.tsx, index.css, App.css

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import OurSheep from './pages/OurSheep';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/our-sheep" element={<OurSheep />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
